a3132d924aa783e86428c1d997e081eb5a719dbe
chih chiu <chih.chiu.19@gmail.com>
2024-12-24 22:16:36 -0800

    save

--- pagerunner/content.js
+++ pagerunner/content.js
@@ -1,3 +1,37 @@
-document.querySelectorAll('p').forEach((paragraph) => {
-  paragraph.style.backgroundColor = "yellow";
-});
+var
+
+var INTERNAL_SEC = 3;
+
+function runUntil(fn, intervalSec, untilSec) {
+  const intervalId = setInterval(() => {
+    console.log('Running periodic function...');
+    fn();
+    console.log('Periodic function completed');
+  }, intervalSec * 1000);
+  setTimeout(function () {
+    clearInterval(intervalId);
+    console.log('Periodic function removed');
+  }, untilSec * 1000);
+}
+
+function contains(str, subStr) {
+  return str.indexOf(subStr) >= 0;
+}
+
+function isRoot() {
+  return window.location.pathname === '/';
+}
+
+function runNextTask() {
+
+}
+
+function executeActionMain() {
+  console.log('hello');
+}
+
+function main() {
+  setInterval(executeActionMain, INTERNAL_SEC * 1000);
+}
+
+main();
